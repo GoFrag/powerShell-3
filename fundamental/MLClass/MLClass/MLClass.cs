0a7e5460c1707fd6fda3baa54222541395fb542b
Ehsan <ehsan5505>
2016-01-19 13:03:41 +0500

    wth is there in fundamental, look at cleaning may be done in the MLCLASS, just neglect this commit in the sake of cleaning up

---
+++ fundamental/MLClass/MLClass/MLClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace MLClass
+{
+    class Message
+    {
+        private bool isRoot { get; set; }
+        private bool isAdmin { get; set; }
+
+        public Message()
+        {
+            isAdmin = true;
+            isRoot = true;
+        }
+
+        public void sendMessage()
+        {
+            System.Diagnostics.Debug.Write("System Administrator is calling");
+            MessageBox.Show("Message", "Message Box is system");
+        }
+
+    }
+}
