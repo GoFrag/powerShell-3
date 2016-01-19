2abfd24533fe5724a10b1aad09c545e4567ebe14
Ehsan <ehsan5505>
2016-01-19 12:34:37 +0500

    This commit include the Net dll object access to the script by the # considering the following steps in the script # After creating the dll class in Visual Basic, perform the following step # # [Reflection.Assembly]:loadFile('path of file') # it show the load conf # then perform the following steps #  = new-object class.object #  | get-member to show the property and method # .method() in order to access the object

---
+++ Class/MLClass/MLClass/Message.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLClass
+{
+    public class Message
+    {
+        public bool isAdmin { get; set; }
+        public bool onDuty { get; set; }
+
+        public Message()
+        {
+            isAdmin = true;
+            onDuty = true;
+        }
+
+        public void sendMsg()
+        {
+            System.Diagnostics.Process.Start("msg", "ehsan A Call from programmer side");
+            MessageBox.Show("System Interface Diagnostics", "This is it");
+        }
+    }
+}
